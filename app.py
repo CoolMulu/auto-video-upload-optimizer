1a3c14f7293cc7bda641bc6ac54cfcd4ee9b6d19
Cool_Mulu <sakura3chanowo@gmail.com>
2026-07-30 17:43:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import streamlit.components.v1 as components
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -8,14 +7,6 @@ st.set_page_config(
     page_title="Auto Video Upload Optimizer",
     page_icon="📊",
     layout="centered"
-)
-
-components.html(
-    """
-    <script data-goatcounter="https://coolmulu.goatcounter.com/count"
-            async src="//gc.zgo.at/count.js"></script>
-    """,
-    height=0,
 )
 
 st.title("📊 Auto Video Upload Optimizer")
